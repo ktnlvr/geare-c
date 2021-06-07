4a10c6c1060443aac57562240fae062301c7eefb
Kittenlover229 <artur.roos@mail.ru>
2021-06-07 21:46:50 +0300

    feat: Construct the engine instance

--- include/engine/app.h
+++ include/engine/app.h
@@ -1,12 +1,35 @@
+#include <malloc.h>
 #include <stdint.h>
 
+#include "../def.h"
+
+// Application descriptor, responsible for all the system-dependent application
+// properties e.g. window title
 typedef struct geare_app_desc_t {
     const char* window_title;
     uint32_t width, height;
     uint64_t flags;
-    uint8_t is_running;
 } geare_app_desc_t;
 
-typedef struct geare_engine_ctx {
+// Contains all the engine data and information
+typedef struct geare_engine_ctx_t {
     geare_app_desc_t desc;
-} geare_engine_ctx;
+    geare_bool is_running;
+} geare_engine_ctx_t;
+
+typedef struct geare_engine_t {
+    // A generic pointer to custom user data
+    void* data;
+    // Engine context
+    geare_engine_ctx_t ctx;
+} geare_engine_t;
+
+static geare_engine_t* _instance;
+
+#define geare_engine_instance() _instance;
+
+// Constructor for the engine instance
+void geare_engine_create(geare_app_desc_t desc) {
+    _instance = (geare_engine_t*)malloc(sizeof(geare_engine_t));
+    _instance->ctx.desc = desc;
+}
--- sandbox/main.c
+++ sandbox/main.c
@@ -1,4 +1,15 @@
-#include "../include/app/app.h"
+#include "../include/engine/app.h"
 
-int main() {}
+int main() {
+    geare_app_desc_t desc = {
+        .window_title = "Test Window",
+        .height = 256,
+        .width = 256,
+    };
 
+    geare_engine_create(desc);
+
+    geare_engine_t* engine_ptr = geare_engine_instance();
+
+    return 0;
+}
